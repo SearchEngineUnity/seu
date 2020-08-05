ee479e9bdc902ff588b5669415dd6e14bf19d54b
holly-searchengineunity <holly@searchengineunity.com>
2020-08-05 16:05:33 -0400

    completed media quries for tiles, completed style of tiles

--- web/src/components/Tile2.js
+++ web/src/components/Tile2.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Card, CardDeck } from 'react-bootstrap';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const PaddedDiv = styled.div`
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 575px) {
+    margin-bottom: 15px;
+  }
+
+  &:nth-last-child(1) {
+    margin-bottom: 1rem;
+  }
+
+  &:nth-last-child(2) {
+    @media screen and (min-width: 576px) {
+      margin-bottom: 1rem;
+    }
+  }
+  &:nth-last-child(3) {
+    @media screen and (min-width: 992px) {
+      margin-bottom: 1rem;
+    }
+  }
+`;
 
 function Tile2({ id, title, image, imageAlt, col, imageFilename }) {
   console.log(id, title, image, imageAlt, col);
   return (
-    <div className={col}>
+    <PaddedDiv className={col}>
       <CardDeck className="">
         <Card key={id} className="border-0">
           {image && (
@@ -22,7 +45,7 @@ function Tile2({ id, title, image, imageAlt, col, imageFilename }) {
           </Card.Body>
         </Card>
       </CardDeck>
-    </div>
+    </PaddedDiv>
   );
 }
 export default Tile2;
