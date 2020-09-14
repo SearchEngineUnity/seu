5559971da0129cba9a38032041e8837628a0b77d
sc-searchengineunity <sharon@searchengineunity.com>
2020-09-14 16:56:16 -0400

    trying preload web font instead

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -55,7 +55,7 @@ const Layout = ({ children }) => {
           crossOrigin="anonymous"
           async
         />
-        <script>
+        {/* <script>
           {`(function(d) {
     var config = {
       kitId: 'mwy2rpa',
@@ -64,11 +64,8 @@ const Layout = ({ children }) => {
     },
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);`}
-        </script>
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Ranchers&display=swap"
-          rel="stylesheet"
-        /> */}
+        </script> */}
+        <link rel="preload" as="font" href="https://use.typekit.net/mwy2rpa.css" />
       </Helmet>
       <GlobalStyle />
       <MainNav {...mapMainNavToProps(data.sanityMainNavbar)} />
